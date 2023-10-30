0007d40f09f268784a6159d2755aa0f84613feb2
YordanovaT <tanyaiyord@gmail.com>
2023-10-30 16:37:47 +0200

    Added contact and index views and templates.

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -1,8 +1,9 @@
 """Module providing the users app urls."""
 
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
+    path('contact/', views.ContactFormView.as_view(), name='contact'),
+    path('', views.IndexView.as_view(), name='base'),
 ]
--- online_shop/views.py
+++ online_shop/views.py
@@ -3,4 +3,27 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 
+
 # Create your views here.
+
+
+class ContactFormView(View):
+    """
+        Class view used for the registration functionality
+        """
+
+    def get(self, request):
+        """ Method used to GET the register page """
+
+        return render(request, 'online_shop/contact.html')
+
+
+class IndexView(View):
+    """
+        Class view used for the registration functionality
+        """
+
+    def get(self, request):
+        """ Method used to GET the register page """
+
+        return render(request, 'users/base.html')
