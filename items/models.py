41436b246987d0509e6cc66a8acea109d4de7dc3
YordanovaT <tanyaiyord@gmail.com>
2023-10-31 15:14:47 +0200

    Added models for items and categories. Created folder for images of items. Updated project urls, front page view and template.

--- items/models.py
+++ items/models.py
@@ -1,4 +1,43 @@
+"""Models for the items app"""
+
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+
+
+class Category(models.Model):
+    """Model class for the item categories"""
+    name = models.CharField(max_length=150)
+
+    class Meta:
+        """
+        Creating class to show categories as plural and to order them by name
+        """
+
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        """Representing the categories by name, not as objects"""
+
+        return self.name
+
+
+class Item(models.Model):
+    """Model class for the items"""
+
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()  # add validation for min and max price
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Representing the items by name, not as objects"""
+
+        return self.name
--- online_shop/views.py
+++ online_shop/views.py
@@ -1,29 +1,32 @@
-"""Django views module"""
+"""Django views module for online_shop app"""
 
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from items.models import Category, Item
 
 
 # Create your views here.
 
 
 class ContactFormView(View):
-    """
-        Class view used for the registration functionality
-        """
+    """ Class view used for the contact form functionality """
 
     def get(self, request):
-        """ Method used to GET the register page """
+        """ Method used to GET the contact page """
 
         return render(request, 'online_shop/contact.html')
 
 
 class IndexView(View):
-    """
-        Class view used for the registration functionality
-        """
+    """ Class view used for the index page """
 
     def get(self, request):
-        """ Method used to GET the register page """
+        """ Method used to GET the index page """
 
-        return render(request, 'users/base.html')
+        context = {}
+        items = Item.objects.filter(is_sold=False)[0:6]
+        context['items'] = items
+        categories = Category.objects.all()
+        context['categories'] = categories
+
+        return render(request, 'users/base.html', context)
