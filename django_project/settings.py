6e38cb02cc3875db3d5cc1441335ae2a97d8df1c
YordanovaT <tanyaiyord@gmail.com>
2023-11-01 12:22:56 +0200

    Updated settings file. Added urls for registration and login.

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,9 +125,11 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuration for needed for the proper visualisation of the base page
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'login'
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 """Module providing the users app urls."""
 
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
+    path('register/', views.RegistrationView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
